8ae520ebe1052600e9022780e51c59a0b9b46456
taikun928 <1663177102@qq.com>
2024-03-21 22:24:31 +0800

    str.hashCode() & 0x7FFFFFFFFFFFFFFFL

--- cdp-segment-web/src/test/java/cn/Main.java
+++ cdp-segment-web/src/test/java/cn/Main.java
@@ -2,12 +2,14 @@ package cn;
 public class Main {
     public static void main(String[] args) {
         String str = "YourStringHere";
+//        String str = "YourStr12121212121221ingHere";
         long hashedValue = generate19DigitLong(str);
         System.out.println("Generated 19-digit long value from \"" + str + "\": " + hashedValue);
     }
 
     public static long generate19DigitLong(String str) {
-        long hash = str.hashCode() & 0xFFFFFFFFFFFFL; // 48 bits
-        return (hash % 9000000000000000000L) + 1000000000000000000L; // Ensure 19 digits
+        long hash = str.hashCode() & 0x7FFFFFFFFFFFFFFFL; // 48 bits
+//        return (hash % 9000000000000000000L) + 1000000000000000000L; // Ensure 19 digits
+        return (hash); // Ensure 19 digits
     }
 }
