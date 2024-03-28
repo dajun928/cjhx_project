087f2a859312e32ad0e8a187ff67e15342a60740
taikun928 <1663177102@qq.com>
2024-03-29 02:13:35 +0800

    SparkReadMysql

--- mdc-big-data/src/main/scala/jdbc/Main.scala
+++ mdc-big-data/src/main/scala/jdbc/Main.scala
@@ -1,51 +1,51 @@
-package jdbc
-
-import java.sql.Connection
-
-trait JdbcConnectionProvider {
-  def getConnection(): Connection
-}
-
-class MyJdbcConnectionProvider extends JdbcConnectionProvider {
-  override def getConnection(): Connection = {
-    // 在这里实现获取数据库连接的逻辑
-    // 例如：使用 DriverManager 获取连接
-    val url = "jdbc:mysql://localhost:3306/cdp"
-    val user = "root"
-    val password = "123456"
-    val connection = java.sql.DriverManager.getConnection(url, user, password)
-    connection
-  }
-}
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    // 创建 MyJdbcConnectionProvider 的实例
-    val connectionProvider = new MyJdbcConnectionProvider()
-
-    // 获取数据库连接
-    val connection = connectionProvider.getConnection()
-
-    try {
-      // 执行数据库操作，这里只是一个示例
-      val statement = connection.createStatement()
-      val resultSet = statement.executeQuery("SELECT * FROM t_user")
-
-      // 处理结果集
-      while (resultSet.next()) {
-        // 处理每一行数据
-        val column1Value = resultSet.getString("user_name")
-        val column2Value = resultSet.getInt("user_age")
-        println(column1Value)
-        println(column2Value)
-
-        // 进行其他操作...
-      }
-    } finally {
-      // 确保在使用完成后关闭数据库连接
-      if (connection != null) {
-        connection.close()
-      }
-    }
-  }
-}
+//package jdbc
+//
+//import java.sql.Connection
+//
+//trait JdbcConnectionProvider {
+//  def getConnection(): Connection
+//}
+//
+//class MyJdbcConnectionProvider extends JdbcConnectionProvider {
+//  override def getConnection(): Connection = {
+//    // 在这里实现获取数据库连接的逻辑
+//    // 例如：使用 DriverManager 获取连接
+//    val url = "jdbc:mysql://localhost:3306/cdp"
+//    val user = "root"
+//    val password = "123456"
+//    val connection = java.sql.DriverManager.getConnection(url, user, password)
+//    connection
+//  }
+//}
+//
+//object Main {
+//  def main(args: Array[String]): Unit = {
+//    // 创建 MyJdbcConnectionProvider 的实例
+//    val connectionProvider = new MyJdbcConnectionProvider()
+//
+//    // 获取数据库连接
+//    val connection = connectionProvider.getConnection()
+//
+//    try {
+//      // 执行数据库操作，这里只是一个示例
+//      val statement = connection.createStatement()
+//      val resultSet = statement.executeQuery("SELECT * FROM t_user")
+//
+//      // 处理结果集
+//      while (resultSet.next()) {
+//        // 处理每一行数据
+//        val column1Value = resultSet.getString("user_name")
+//        val column2Value = resultSet.getInt("user_age")
+//        println(column1Value)
+//        println(column2Value)
+//
+//        // 进行其他操作...
+//      }
+//    } finally {
+//      // 确保在使用完成后关闭数据库连接
+//      if (connection != null) {
+//        connection.close()
+//      }
+//    }
+//  }
+//}
